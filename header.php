f60c8bc8ef589f210432e82f77900a30607f9bea
Semir Smajlovic <semir.smajlovic@fleishman.com>
2020-09-02 14:45:38 -0500

    Add needed files to render WordPress core

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="is-fullheight">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+
+    <?php wp_head(); ?>
+</head>
+<body>
+
+</body>
+</html>
