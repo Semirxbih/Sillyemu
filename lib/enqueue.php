c57f9d4f13ed08d12830fa0de0b9c85c2528e1d2
Semir Smajlovic <semir.smajlovic@fleishman.com>
2020-09-02 20:29:16 -0500

    Enqueue the Bulma framework

--- lib/enqueue.php
+++ lib/enqueue.php
@@ -1,1 +1,19 @@
 <?php
+/**
+ * Scripts and Styles
+ *
+ * This file contains functions that will enqueue scripts and styles
+ *
+ * @package Sillyemu
+ * @version 1.0
+ */
+
+if (!function_exists('sillyemu_scripts')) {
+    /**
+     * Enqueue scripts and styles
+     */
+    function sillyemu_scripts() {
+        wp_enqueue_style('bulma-style', get_template_directory_uri() . '/node_modules/bulma/css/bulma.css', [], time(), '');
+    }
+}
+add_action('wp_enqueue_scripts', 'sillyemu_scripts');
