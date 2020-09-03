edae8318a2e739682fbadf207e1dc570b992e560
Semir Smajlovic <semir.smajlovic@fleishman.com>
2020-09-02 22:25:46 -0500

    Add support for screen readers

--- lib/functions.php
+++ lib/functions.php
@@ -6,3 +6,10 @@
 if (true == get_theme_mod('admin_panel_adminbar', false)) {
     show_admin_bar(false);
 }
+
+if (!function_exists('sillyemu_screen_reader_text')) {
+    function sillyemu_screen_reader_text() {
+        $output = '<a class="skip-link screen-reader-text" href="#content">Skip to content</a>';
+        echo $output;
+    }
+}
