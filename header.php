abffd51a11fbcc14eb65d43da27efc1435cec3bf
Semir Smajlovic <semir.smajlovic@fleishman.com>
2020-09-10 13:06:11 -0500

    Update the wp body class

--- header.php
+++ header.php
@@ -12,27 +12,76 @@
 
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> class="is-fullheight">
+
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
     <?php wp_head(); ?>
 </head>
+
 <body <?php body_class() ?>>
-    <div id="page" class="site">
-<!--        --><?php //sillyemu_screen_reader_text(); ?>
-        <header id="header" class="nav-down">
-            <nav id="site-navigation" class="navbar container" role="navigation">
+
+        <!-- Outputting scripts that must come immediately after body -->
+        <?php if (function_exists('wp_body_open')) {
+            wp_body_open();
+        } ?>
+
+        <header id="header" style="background-color: skyblue">
+
+            <nav class="navbar container" style="background-color: lightyellow">
                 <div class="navbar-brand">
+                    <?php sillyemu_logo('navbar-item', get_custom_logo()); ?>
                     <?php sillyemu_navbar_burger(); ?>
                 </div>
-                <div class="navbar-menu">
+
+                <div id="navbarExampleTransparentExample" class="navbar-menu">
                     <div class="navbar-start">
                         <p>Test</p>
                     </div>
+
+                    <div class="navbar-end">
+                        <div class="navbar-item">
+                            <a class="navbar-item" href="https://bulma.io/">
+                                Home
+                            </a>
+                            <div class="navbar-item has-dropdown is-hoverable">
+                                <a class="navbar-link" href="https://bulma.io/documentation/overview/start/">
+                                    Docs
+                                </a>
+                                <div class="navbar-dropdown is-boxed">
+                                    <a class="navbar-item" href="https://bulma.io/documentation/overview/start/">
+                                        Overview
+                                    </a>
+                                    <a class="navbar-item" href="https://bulma.io/documentation/overview/modifiers/">
+                                        Modifiers
+                                    </a>
+                                    <a class="navbar-item" href="https://bulma.io/documentation/columns/basics/">
+                                        Columns
+                                    </a>
+                                    <a class="navbar-item" href="https://bulma.io/documentation/layout/container/">
+                                        Layout
+                                    </a>
+                                    <a class="navbar-item" href="https://bulma.io/documentation/form/general/">
+                                        Form
+                                    </a>
+                                    <hr class="navbar-divider">
+                                    <a class="navbar-item" href="https://bulma.io/documentation/elements/box/">
+                                        Elements
+                                    </a>
+                                    <a class="navbar-item is-active" href="https://bulma.io/documentation/components/breadcrumb/">
+                                        Components
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </nav>
+
+
         </header>
 
-        <div id="content" class="site-content">
+
+
+        <div id="content" class="site-content" style="background-color: #00a699">
