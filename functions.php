e6f74b7dc1979c4575133f2ea79d7ca347c3a7a0
Semir Smajlovic <semir.smajlovic@fleishman.com>
2020-09-10 19:58:28 -0500

    Refactor the functions.php

--- functions.php
+++ functions.php
@@ -11,11 +11,3 @@
  */
 
 require_once __DIR__ . '/lib/init.php';
-
-if (!function_exists('sillyemu_setup')) {
-    function sillyemu_setup() {
-        require get_template_directory() . '/lib/enqueue.php';
-        require get_template_directory() . '/lib/functions.php';
-    }
-}
-add_action('after_setup_theme', 'sillyemu_setup');
